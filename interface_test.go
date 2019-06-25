acee8a3d40b8169b63bcfd3abcc0684899df67fb
Daniel Fanjul <daniel.fanjul.alcuten@gmail.com>
2019-06-25 11:48:40 +0200

    Add more interfaces

--- interface_test.go
+++ interface_test.go
@@ -7,19 +7,15 @@ import (
 
 func TestTableCanBeSerializedAsJson(t *testing.T) {
 	r := Table{
-		Name:         "foo",
-		Fields:       map[FieldName]FieldType{"bar": "string", "baz": "int"},
-		PrimaryKey:   []FieldName{"bar"},
-		PartitionKey: []FieldName{"bar"},
-		PrimaryKeyCache: TablePrimaryKeyCache{
-			Enabled:      true,
-			CachePrefix:  "foo_pk",
-			CacheVersion: 0,
-		},
+		Name:                  "foo",
+		Fields:                map[FieldName]FieldType{"bar": "string", "baz": "int"},
+		PrimaryKey:            []FieldName{"bar"},
+		PartitionKey:          []FieldName{"bar"},
+		PrimaryKeyCachePrefix: CacheKey{"foo_pk", "0"},
 	}
 	if b, err := json.Marshal(r); err != nil {
 		t.Fatal(err)
-	} else if s := string(b); s != `{"name":"foo","fields":{"bar":"string","baz":"int"},"primaryKey":["bar"],"partitionKey":["bar"],"primaryKeyCache":{"enabled":true,"cachePrefix":"foo_pk","cacheVersion":0}}` {
+	} else if s := string(b); s != `{"name":"foo","fields":{"bar":"string","baz":"int"},"primaryKey":["bar"],"partitionKey":["bar"],"primaryKeyCachePrefix":["foo_pk","0"]}` {
 		t.Error(s)
 	}
 }
